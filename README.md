d6e1ce53f955a403f9d2ae2b3c02c4fd28a31ca1
BangL <henno.rickowski@googlemail.com>
2012-10-21 03:02:18 +0200

    - description updated - plugin.yml updated - plugin name corrected - cleanup

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 WGTreeGrowFlags
 ===============
+
+WorldGuard extension to deny tree growing in regions
+or to deny growing of leaves in a region if the tree is based in another region.
+
 Adds two new flags to WorldGuard regions:
-tree-grow: allow/deny tree growing inside a region
-leave-grow: allow/deny growing of leaves in a region if the tree comes from an other region
+tree-grow: allow/deny tree growing
+leave-grow: allow/deny growing of leaves
---
+++ com/mewin/WGTreeGrowFlags/GrowListener.java
@@ -0,0 +1,96 @@
+package com.mewin.WGTreeGrowFlags;
+
+import com.mewin.WGCustomFlags.WGCustomFlagsPlugin;
+import com.sk89q.worldguard.bukkit.WorldGuardPlugin;
+import com.sk89q.worldguard.protection.ApplicableRegionSet;
+import com.sk89q.worldguard.protection.flags.StateFlag.State;
+import com.sk89q.worldguard.protection.managers.RegionManager;
+import com.sk89q.worldguard.protection.regions.ProtectedRegion;
+import java.lang.reflect.Field;
+import java.util.Collection;
+import java.util.Iterator;
+import org.bukkit.ChatColor;
+import org.bukkit.block.BlockState;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.world.StructureGrowEvent;
+
+/**
+ *
+ * @author mewin
+ */
+public class GrowListener implements Listener {
+    private WGCustomFlagsPlugin custPlugin;
+    private WorldGuardPlugin wgPlugin;
+    private WGTreeGrowFlagsPlugin plugin;
+    
+    public GrowListener(WGTreeGrowFlagsPlugin plugin, WorldGuardPlugin wgPlugin, WGCustomFlagsPlugin custPlugin)
+    {
+        this.custPlugin = custPlugin;
+        this.wgPlugin = wgPlugin;
+        this.plugin = plugin;
+    }
+    
+    @EventHandler
+    public void onStructureGrow(StructureGrowEvent e)
+    {
+        RegionManager rm = wgPlugin.getRegionManager(e.getWorld());
+        
+        if (rm == null)
+        {
+            return;
+        }
+        
+        ApplicableRegionSet regions = rm.getApplicableRegions(e.getLocation());
+        Collection<ProtectedRegion> regCol = (Collection<ProtectedRegion>) getPrivateValue(regions, "applicable");
+        
+        if (!regions.allows(WGTreeGrowFlagsPlugin.TREE_GROW_FLAG))
+        {
+            if (!e.isFromBonemeal() || !e.getPlayer().isOp())
+            {
+                e.setCancelled(true);
+                if (e.getPlayer() != null)
+                {
+                    e.getPlayer().sendMessage(ChatColor.RED + "You may not grow trees here.");
+                }
+            }
+        }
+        
+        Iterator<BlockState> itr = e.getBlocks().iterator();
+        if (e.getPlayer() == null || !e.getPlayer().isOp())
+        {
+            while(itr.hasNext())
+            {
+                BlockState state = itr.next();
+
+                ApplicableRegionSet blockRegions = rm.getApplicableRegions(state.getLocation());
+                Iterator<ProtectedRegion> itr2 = blockRegions.iterator();
+
+                while (itr2.hasNext())
+                {
+                    ProtectedRegion region = itr2.next();
+                    State flagState = region.getFlag(WGTreeGrowFlagsPlugin.LEAVE_GROW_FLAG);
+
+                    if (flagState == State.DENY && !regCol.contains(region))
+                    {
+                        itr.remove();
+                    }
+                }
+            }
+        }
+    }
+    
+    private Object getPrivateValue(Object obj, String name)
+    {
+        try
+        {
+            Field f = obj.getClass().getDeclaredField(name);
+            f.setAccessible(true);
+            return f.get(obj);
+        }
+        catch(NoSuchFieldException | SecurityException | IllegalArgumentException | IllegalAccessException ex)
+        {
+            return null;
+        }
+    }
+}
